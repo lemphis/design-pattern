3b9b8703c9d58e55fa0500e7490d7fbe58d73da5
Jaeho Jang <jaesone@douzone.com>
2020-11-22 15:48:22 +0900

    Add strategy pattern

---
+++ strategy/MainClass.java
@@ -0,0 +1,9 @@
+package strategy;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+    }
+}
