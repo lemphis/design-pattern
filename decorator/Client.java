df415d67bf398c8cd54eb80f6d322627d8d7eed4
lemphis <lemphis@gmail.com>
2022-10-01 17:38:26 +0900

    Add decorator pattern

---
+++ decorator/Client.java
@@ -0,0 +1,15 @@
+package decorator;
+
+public class Client {
+
+	private final CommentService commentService;
+
+	public Client(CommentService commentService) {
+		this.commentService = commentService;
+	}
+
+	public void writeComment(String comment) {
+		commentService.addComment(comment);
+	}
+
+}
